37c1eeaf248c994f3d558f6aed5b076343a8cf72
Khadarrr <111360349+Khadarrr@users.noreply.github.com>
2023-04-20 15:16:23 +0200

    Blog P added

---
+++ app.js
@@ -0,0 +1,33 @@
+const blogContainer = document.querySelector("#blog-post");
+const url = "http://ca-cms.local/wp-json/wp/v2/posts/"
+
+async function fetchBloggPost() {
+    try {
+        const response = await fetch(url);
+        const json = await response.json();
+       
+
+        let html = "";
+        json.forEach(item => {
+            html += `
+            <div class="post">
+            
+            <div class="content">
+              ${item.title.rendered}
+              <p class="date">${new Date(item.date).toLocaleDateString()}</p>
+              <p class="excerpt">${item.excerpt.rendered}</p>
+              <a href="${item.link}" class="read-more">Read More</a>
+            </div>
+          </div>
+          `;
+        });
+
+        blogContainer.innerHTML = html;
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+fetchBloggPost()
+
